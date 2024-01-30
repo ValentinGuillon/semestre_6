81cccbd55c23642ad94da286df8ca26e7125ee0b
Valentin_G <valentin.guillon93430@gmail.com>
2024-01-31 00:13:27 +0100

    iaa, tp2: complete (sauf silhouette)

--- 1_ia_et_apprentissage/tp2/tp2.py
+++ 1_ia_et_apprentissage/tp2/tp2.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-K = 6 #max 10 (for the color)
-DATAS_SRC = "random" #"iris" or "random"
-WITH_GRAPH = True #with True, one time alog then graph. with False (always "iris" datas), test 100 times the algo for 1 to K clusters.
-DIST_METHOD = "minkowski" #"manhattan" or "euclidian" or "minkowski"
+K = 4 #max 10 (for the color)
+DATAS_SRC = "iris" #"iris" or "random"
+WITH_TESTS = True #with True, test x times the algo for 1 to K clusters.
+WITH_GRAPH = True #with True, one time alog then graph.
+DIST_METHOD = "euclidian" #"manhattan" or "euclidian" or "minkowski"
 MINKOWSKI_VALUE = 3 #1=> manhattan, 2=> euclidian
 
 ALLOW_PRINT_DEBUG = False
@@ -14,47 +15,47 @@ SHOW_CENTER_INIT_POS = False
 
 
 
-#describe a point with any number of coordinates
+#describe a point with any number of coordinates, associate to a cluster, and an optional group (to calculate cluster's purity)
 class Point:
-    def __init__(self, coords: list[float]):
+    pass #just to use its name inside itself
+class Point:
+    def __init__(self, coords: list[float], group: any = 0):
         self.coords = coords
-    
+        self.cluster = -1 #is's cluster this point is attached
+
+        self.group = group
+
+
+    def __str__(self):
+        return f"group:<{self.group}>, cluster:<{self.cluster}>, <{self.coords}>"
+
+
     @staticmethod
-    def average(nb_coords, points: list):
+    def average(nb_coords, points: list[Point]) -> list[float]:
         nb_points = len(points)
         if nb_points == 0:
             return points
 
-        result = [0 for i in range(nb_coords)]
+        results = [0 for _ in range(nb_coords)]
         
         for point in points:
             if len(point.coords) != nb_coords:
                 print("error.\nIn Point.average().\nThe point has a different number of coordinate then expected.")
                 return None #throw error
             for i in range(nb_coords):
-                result[i] += point.coords[i]
+                results[i] += point.coords[i]
         
-        res = [(val / nb_points) for val in result]
-        return res
+        final_res = [(val / nb_points) for val in results]
+        return final_res
 
-    @staticmethod
-    # return 2 coords set where one has all values as the minimum existing, in points, and the other with the maximum.
-    def min_max(nb_coords, points: list):
-        min = points[0].coords.copy()
-        max = points[0].coords.copy()
-        
-        for point in points:
-            if len(point.coords) != nb_coords:
-                #raise error
-                print("error.\nIn Point.min_max().\nSome points have different coordinates number.")
 
-            for i, val in enumerate(point.coords):
-                if val < min[i]:
-                    min[i] = val
-                if val > max[i]:
-                    max[i] = val
+    #return True if self.cluster has changed
+    def update_cluster(self, new_cluster: int) -> bool:
+        if self.cluster != new_cluster:
+            self.cluster = new_cluster
+            return True
+        return False
 
-        return min, max
 
     
     def dist_euclidian(self, other_point):
@@ -66,14 +67,15 @@ class Point:
     
     
     #dist minkowski
-    def dist(self, other_point):
-        p = 2 #p=1 => Manhattan, p=2 => euclidian
-        if DIST_METHOD == "manhattan":
-            p = 1
-        elif DIST_METHOD == "euclidian":
-            p = 2
-        elif DIST_METHOD == "minkowski":
-            p = MINKOWSKI_VALUE
+    def dist(self, other_point, p = 0):
+        #p=1 => Manhattan, p=2 => euclidian
+        if p <= 0:
+            if DIST_METHOD == "manhattan":
+                p = 1
+            elif DIST_METHOD == "euclidian":
+                p = 2
+            elif DIST_METHOD == "minkowski":
+                p = MINKOWSKI_VALUE
     
         nb_coords = len(self.coords)
         other_coords = other_point.coords
@@ -93,24 +95,24 @@ class Point:
         return dist
         
 
-#Coordinates set associate with a ummutable group, and a variable cluster
-class Flower(Point):
-    def __init__(self, group: int, coords: list[int]):
-        super().__init__(coords)
-        self.group = group
-        self.cluster = 0
+# #Coordinates set associate with a ummutable group, and a variable cluster
+# class Flower(Point):
+#     def __init__(self, group: int, coords: list[int]):
+#         super().__init__(coords)
+#         self.group = group
+#         self.cluster = 0
 
 
-    def __str__(self):
-        return f"group:<{self.group}>, cluster:<{self.cluster}>, <{self.coords}>"
+#     def __str__(self):
+#         return f"group:<{self.group}>, cluster:<{self.cluster}>, <{self.coords}>"
     
     
-    #return True if self.cluster has changed
-    def update_cluster(self, new_cluster: int) -> bool:
-        if self.cluster != new_cluster:
-            self.cluster = new_cluster
-            return True
-        return False
+#     #return True if self.cluster has changed
+#     def update_cluster(self, new_cluster: int) -> bool:
+#         if self.cluster != new_cluster:
+#             self.cluster = new_cluster
+#             return True
+#         return False
 
 
 #Coordinates set associate with a immutable id, and a variable nb of point set to this cluster
@@ -127,6 +129,32 @@ class Cluster(Point):
         res += f"{self.coords[-1]:.2f}]>"
             
         return res
+    
+    # return the average of the distance between all possible data, set to this cluster
+    def similarity_intra(self, datas: list[Point]) -> float:
+        cluster_points = [point for point in datas if point.cluster == self.id]
+
+        total = 0
+
+        for i, p1 in enumerate(cluster_points[0: -1]):
+            for p2 in cluster_points[i+1: -1]:
+                total += p1.dist(p2)
+        
+        return total
+
+
+
+
+
+
+
+def SSE(clusters: list[Cluster], datas: list[Point]) -> float:
+    res = 0.0
+    for cluster in clusters:
+        ssec = cluster.similarity_intra(datas)
+        res += ssec
+    
+    return res
 
 
 
@@ -141,25 +169,22 @@ class Cluster(Point):
 
 
 
-
-
-
-
-
-
-def extract_datas() -> dict:
+def extract_datas(file_datas: str, file_groups: str = "") -> dict:
     datas: list[Point] = []
+    # with_group = True
+    # if not file_groups:
+    #     with_group = False
     
     datas_label = []
-    with open("iris_label.csv", "r") as file:
+    with open(file_groups, "r") as file:
         for i, line in enumerate(file):
             label = int(line.split(";")[0])
             datas_label.append(label)
 
-    with open("iris_data.csv", "r") as file:
+    with open(file_datas, "r") as file:
         for i, line in enumerate(file):
             temp = line.split(";")[0:-1]
-            data = Flower(datas_label[i], [float(val) for val in temp])
+            data = Point([float(val) for val in temp], datas_label[i])
             datas.append(data)
 
     return datas
@@ -170,31 +195,31 @@ def generate_datas() -> dict:
     # C1 ---------->
     centre1=np.array([3,3])            # centre du premier cluster
     sigma1=np.array([[1.5,0],[0,1.5]]) # matrice de covariance du premier cluster
-    taille1=250                        # nombre de points du premier cluster
+    taille1=25                        # nombre de points du premier cluster
     cluster1=np.random.multivariate_normal(centre1,sigma1,taille1)
     # C1 <----------
     # C2 ---------->
     centre2=np.array([-3,-3])          # centre du second cluster
     sigma2=np.array([[1.5,0],[0,1.5]]) # matrice de covariance du second cluster
-    taille2=200                        # nombre de points du second cluster
+    taille2=25                        # nombre de points du second cluster
     cluster2=np.random.multivariate_normal(centre2,sigma2,taille2)
     # C2 <----------
     # C3 ---------->
     centre3=np.array([-3,3])          # centre du troisieme cluster
     sigma3=np.array([[1.5,0],[0,1.5]]) # matrice de covariance du second cluster
-    taille3=300                        # nombre de points du second cluster
+    taille3=25                        # nombre de points du second cluster
     cluster3=np.random.multivariate_normal(centre3,sigma3,taille3)
     # C3 <----------
     # C4 ---------->
     centre4=np.array([3,-3])          # centre du quatrieme cluster
     sigma4=np.array([[1.5,0],[0,1.5]]) # matrice de covariance du second cluster
-    taille4=150                        # nombre de points du second cluster
+    taille4=25                        # nombre de points du second cluster
     cluster4=np.random.multivariate_normal(centre4,sigma4,taille4)
     # C4 <----------
 
 
     clusters=np.concatenate((cluster1, cluster2, cluster3, cluster4))
-    to_ret = [Flower(0, coords) for coords in clusters]
+    to_ret = [Point(coords, 0) for coords in clusters]
     
     return to_ret
 
@@ -204,18 +229,9 @@ def generate_datas() -> dict:
 
 def create_centers(nb, datas: list[Point]) -> list[Cluster]:
     centers: list[Point] = []
-    # min, max = Point.min_max(len(datas[0].coords), datas)
 
+    # choose randomly an existing point
     for i in range(nb):
-        # each coords random
-        #randoms_coords = []
-        #for j in range(len(datas[0].coords)):
-        #    #... WIP. r must be a float value
-        #    r = np.random.default_rng().integers(low=min[j], high=max[j], size=1)
-        #    randoms_coords.append(r)
-        #center = Cluster(i+1, randoms_coords)
-        
-        # choose randomly an existing point
         index = np.random.randint(0, len(datas))
         copied_coords = datas[index].coords.copy()
         center = Cluster(i+1, copied_coords)
@@ -226,7 +242,7 @@ def create_centers(nb, datas: list[Point]) -> list[Cluster]:
 
 
 
-def assign_datas_to_clusters(datas, clusters) -> bool:
+def assign_datas_to_clusters(datas: list[Point], clusters: list[Cluster]) -> bool:
     a_data_changed_of_cluster = False
 
     #reset nb of points of clusters
@@ -272,10 +288,11 @@ def update_clusters_coords(datas: list[Point], clusters: list[Cluster]):
 
 
 
-# return the proportion of the most prensent group if the cluster
-def cluster_purity(cluster_id: int, cluster_size: int, datas: list[Flower]) -> float:
-    group_concentration = {} #key=Flower.group, value=occurence in datas
+# return the proportion of the most present group in the cluster
+def cluster_purity(cluster_id: int, cluster_size: int, datas: list[Point]) -> float:
+    group_concentration = {} #key=Point.group, value=occurence in datas
 
+    #count
     for point in datas:
         if point.cluster != cluster_id:
             continue
@@ -286,11 +303,13 @@ def cluster_purity(cluster_id: int, cluster_size: int, datas: list[Flower]) -> f
         else:
             group_concentration[group] += 1
 
+    #search the greater proportion
     max = 0
     for key, value in group_concentration.items():
         if value > max:
             max = group_concentration[key]
     
+    # return
     if max == 0:
         return 0
     return (max / cluster_size) *100
@@ -299,7 +318,7 @@ def cluster_purity(cluster_id: int, cluster_size: int, datas: list[Flower]) -> f
 
 
 
-def global_purity(clusters: list[Cluster], datas: list[Flower]) -> float:
+def global_purity(clusters: list[Cluster], datas: list[Point]) -> float:
     purity = 0
 
     for cluster in clusters:
@@ -328,36 +347,49 @@ def algo(clusters: list[Cluster], datas: list[Point]):
 
     
 def found_better_k(datas: list[Point], k_max: int):
+    nb_tests = 10
     purities = {}
     clusters = []
+    sses = {}
 
     for i in range(k_max+1):
         if i < 1:
             continue
 
         average_purity = 0
-        for j in range(100):
+        average_sse = 0
+        for j in range(nb_tests):
             clusters.clear()
             clusters = create_centers(i, datas)
             algo(clusters, datas)
             purity = global_purity(clusters, datas)
+            sse = SSE(clusters, datas)
             average_purity += purity
-            if ALLOW_PRINT_DEBUG and DATAS_SRC == "iris":
-                print(f"test {j}. nb clusters = {i}, purity = {purity}")
+            average_sse += sse
+            if ALLOW_PRINT_DEBUG:
+                print(f"test {j}. nb clusters = {i}, purity = {purity}, sse = {sse}")
 
-        average_purity /= 100
+        average_purity /= nb_tests
+        average_sse /= nb_tests
         purities[i] = average_purity
-        if DATAS_SRC == "iris":
-            print(f"nb clusters = {i}, average purity = {average_purity}")
+        sses[i] = average_sse
+        if ALLOW_PRINT_DEBUG:
+            print(f"nb clusters = {i}, average purity = {average_purity}, av sse = {average_sse}")
 
     best = 0
     greater = 0
     for key, value in purities.items():
-        if ALLOW_PRINT_DEBUG and DATAS_SRC == "iris":
-            print(f"k={key}, purity={value}.")
+        print(f"k={key}, purity={value}, sse = {sses[key]}.")
         if value > greater:
             greater = value
             best = key
+    
+    plt.clf()
+    for key, val in sses.items():
+        plt.scatter([key], [val], color="black", s=50)
+    
+    plt.show()
+    plt.clf()
 
     return best
 
@@ -381,19 +413,21 @@ def main():
     print("ALLOW_PRINT_DEBUG =", ALLOW_PRINT_DEBUG)
     print("SHOW_CENTER_INIT_POS =", SHOW_CENTER_INIT_POS)
     
-    _ = input("\nPress Enter to proceed...")
+    input("\nPress Enter to proceed...")
     
 
 
-    datas:list[Flower] = []
+    datas:list[Point] = []
     if DATAS_SRC == "iris":
-        datas = extract_datas()
+        datas = extract_datas("iris_data.csv", "iris_label.csv")
     elif DATAS_SRC == "random":
         datas = generate_datas()
 
-    if not WITH_GRAPH and DATAS_SRC == "iris":
+    if WITH_TESTS:
         better_k = found_better_k(datas, K)
         print(f"Better nb of clusters (greater purity): {better_k}")
+    
+    if not WITH_GRAPH:
         return
 
 
